c3cd9521b5d4fae99af8ff2c975caa51d9845094
ivch <chebiva@gmail.com>
2021-11-21 01:32:56 -0800

    cors issues

--- components/Times.jsx
+++ components/Times.jsx
@@ -7,7 +7,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function Times({ abbr, index, clickedIndex }) {
 	console.log(`index: ${index} clickedindex: ${clickedIndex}`);
 	if (index !== clickedIndex) return '';
-	const stationsUrl = 'https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y';
+
 	const departureTimeUrl = `http://api.bart.gov/api/etd.aspx?cmd=etd&key=MW9S-E7SL-26DU-VV8V&json=y&orig=${abbr}`;
 
 	//sconst { data, error } = useSWR(stationsUrl, fetcher);
