b59bbfa105b81c3c8dfde9cd1b7f8813e3cffd18
ivch <chebiva@gmail.com>
2021-11-21 02:22:55 -0800

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const withPWA = require('next-pwa');
 module.exports = {
 	reactStrictMode: true,
+	pwa: {
+		dest: 'public',
+		register: true,
+		skipWaiting: true
+	},
 	async headers() {
 		return [
 			{
